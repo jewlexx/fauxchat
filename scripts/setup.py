5c1ad5d8b70395702be1d75f3bf054ed48ed672e
Juliette Cordor <juliette@jewelexx.com>
2023-04-21 23:28:11 +1000

    remove python scripts

--- scripts/setup.py
+++
@@ -1,8 +0,0 @@
-""" Setup environment by initializing submodules and applying patches """
-from os import chdir, system
-
-system("git submodule update --init --recursive")
-
-chdir("chat")
-
-system("git apply ../patches/jChat.patch")
