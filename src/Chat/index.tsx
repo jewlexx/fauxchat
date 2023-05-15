699f770983df83721a045fb276f4a5bd670ad8c9
Juliette Cordor <juliette@jewelexx.com>
2023-05-15 10:03:04 +1000

    fi

--- src/Chat/index.tsx
+++ src/Chat/index.tsx
@@ -1,4 +1,4 @@
-import { type FunctionComponent, useState } from 'react';
+import { type FunctionComponent } from 'react';
 import { Card } from '@mui/material';
 import Iframe from 'react-iframe';
 import { invoke } from '@tauri-apps/api/tauri';
@@ -13,7 +13,7 @@ interface Props {
 const Chat: FunctionComponent<Props> = ({ port, url = '127.0.0.1', path }) => {
   return (
     <Card>
-      <Iframe url={`http://${url}:${port}${path}`} />
+      <Iframe url={`http://${url}:${port}${path}`} frameBorder={0} />
     </Card>
   );
 };
