7750bea675cafbf54a0b2596f8f76b22aca839fa
Juliette Cordor <juliette@jewelexx.com>
2023-06-22 14:29:24 +1000

    added load_file tauri command

--- src-tauri/src/tcmds.rs
+++ src-tauri/src/tcmds.rs
@@ -1,4 +1,10 @@
-use commands::Command;
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
+
+use commands::{Command, CommandsError};
 
 use crate::ready_message;
 
@@ -6,6 +12,9 @@ use crate::ready_message;
 pub enum CommandError {
     #[error("Failed to interact with system IO: {0}")]
     IOError(#[from] std::io::Error),
+
+    #[error("Failed to parse command: {0}")]
+    ParseError(#[from] CommandsError),
 }
 
 impl serde::Serialize for CommandError {
@@ -21,22 +30,30 @@ impl serde::Serialize for CommandError {
 type Result<T> = std::result::Result<T, CommandError>;
 
 #[tauri::command]
-pub fn invoke_command(command: &str, username: Option<&str>) {
+pub fn invoke_command(command: &str, username: Option<&str>) -> Result<()> {
     info!("Invoking command: {}", command);
 
     let username = username.unwrap_or("random").to_string();
 
     // TODO: Better error handling
-    let parsed = Command::try_from(command.to_string()).expect("valid command");
+    let parsed = Command::try_from(command.to_string())?;
 
     ready_message((parsed, username));
+
+    Ok(())
 }
 
 #[tauri::command]
 pub fn load_file(path: &str) -> Result<()> {
-    use std::{fs::File, io::Read};
+    let lines = read_lines(path)?;
 
-    let file = File::open(path)?;
+    for line in lines {
+        let parsed = Command::try_from(line?)?;
+
+        // TODO: Replace random with actual user-defined username
+        // TODO: (preferably implement a way to provide username in the command)
+        ready_message((parsed, "random".to_string()));
+    }
 
     Ok(())
 }
@@ -52,4 +69,12 @@ pub fn send_message(message: &str, username: &str, count: usize, delay: u64) {
     };
 
     ready_message((command, username.to_string()));
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
