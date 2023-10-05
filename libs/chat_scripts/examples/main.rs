d30a8886e45ae9ebfe558bf037ba1b5fd596b63d
Juliette Cordor <juliette@jewelexx.com>
2023-10-06 09:48:22 +1100

    fixes

--- libs/chat_scripts/examples/main.rs
+++ libs/chat_scripts/examples/main.rs
@@ -4,7 +4,9 @@ use chat_scripts::ChatScripts;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let main_module_path = PathBuf::from(file!()).with_extension(".js");
+    let main_module_path =
+        PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "\\examples\\main.rs"))
+            .with_extension("js");
     let mut scripts = ChatScripts::new(|| {}).unwrap();
     scripts.load_module(main_module_path)?;
 
